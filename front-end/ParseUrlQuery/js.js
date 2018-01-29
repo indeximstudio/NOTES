2a00b225e0d2fb28ff16018c895174dc85ae6104
admin <kotenko.serhii@yandex.ua>
2018-01-29 16:50:45 +0200

    new ф-цыя дял парсинга параметров строки в масив

---
+++ front-end/ParseUrlQuery/js.js
@@ -0,0 +1,11 @@
+function parseUrlQuery() {
+    var data = {};
+    if(location.search) {
+        var pair = (location.search.substr(1)).split('&');
+        for(var i = 0; i < pair.length; i ++) {
+            var param = pair[i].split('=');
+            data[param[0]] = param[1];
+        }
+    }
+    return data;
+}
